27cecef0f98f4468a45135205016c659d2fbea2a
Andrew T <metkraming@gmail.com>
2020-01-23 12:45:47 +0200

    add datatypes.js file

---
+++ part1/datatypes.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//js - Динамически типизированный, переменная может хранить разные типы данных
+
+//Number (we can * /  + -)
+let numInt = 24;
+let numFlt = 14.34;
+//We have also Infinity, -Infinity, NaN
+
+//String '' == "". Also we have `` (press tilda) e.g. alert(`This is ${numInt}`)
+let name = "User Name";
+
+//Boolean
+let isItRed = true;
+let isEarthFlat = false;
+
+//null - stores only null. It's like nothing or emptyness
+let netNullVar = null;
+
+//undefined is working like checker - has variable been defined
+
+//typeOf - return type of variable, e.g. typeOf(numInt) will return "number"
