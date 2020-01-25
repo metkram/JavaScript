3cce258df5e16c90edfd23f51d49b7ceb6ffb4da
Andrew T <metkraming@gmail.com>
2020-01-25 11:25:37 +0200

    add alert_prompt_confirm.js file

---
+++ part1/2/alert_prompt_confirm.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//alert(value);
+
+//prompt(title, [default]) return default value or typed by user or null is nothint is typed
+let age = prompt("How old are you?", 2);
+alert(`You are ${age} years old`);
+
+//confirm(question); return true or false
+let aliveStatus = confirm("Are you alive?");
+alert(`You alive status is ${aliveStatus}`);
