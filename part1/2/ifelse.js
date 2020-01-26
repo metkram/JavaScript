cb0ba497a69781e96b23d0cca48589e3044549c0
Andrew T <metkraming@gmail.com>
2020-01-26 10:59:12 +0200

    add ifelse.js file

---
+++ part1/2/ifelse.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// if (true) {
+//   alert("It\'s true");
+// } else {
+//   alert("It is false");
+// }
+//
+// let someInt = 10;
+//
+// if (someInt < 10) {
+//   alert(false);
+// } else if (someInt > 10) {
+//   alert(false);
+// } else {
+//   alert(true);
+// }
+
+let newNumber = prompt("Enter a Number", 0);
+if (newNumber == 0) {
+  alert(0);
+} else if (newNumber > 0) {
+  alert(1);
+} else {
+  alert(-1);
+}
