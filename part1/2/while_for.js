4c16fc6c5c55191b58e3096d623578d4dc489945
Andrew T <metkraming@gmail.com>
2020-01-27 21:44:42 +0200

    add while_for.js file

---
+++ part1/2/while_for.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//while
+
+// while (true) {
+//   //some code
+// }
+
+//do..while
+
+// do {
+//   //some code
+// } while (true);
+
+//for
+
+// for(let i = 0; i < 3; i++) {
+//   //some code
+// }
+
+// for(let i = 0; i < 3; i++) {
+//   if (i = 2) {
+//     break; // break the cycle
+//   }
+// }
+
+// for(let i = 0; i < 3; i++) {
+//   if (i = 2) {
+//     continue; // stop this and go to next iteration
+//   }
+// }
+
+// firstCycle: for (var i = 0; i < 10; i++) {
+//   for (var j = 0; j < 10; i++) {
+//     if (j = 5) {
+//       break firstCycle; //break two cycles (not only the inner)
+//     }
+//   }
+// }
