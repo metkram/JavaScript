260eb77703a40d6788803ebd7fdaa7281696bba5
unknown <metkraming@gmail.com>
2020-01-28 13:41:25 +0200

    add README.md and switch.js files

---
+++ README.md
@@ -0,0 +1,3 @@
+I've tried a couple times to learn JS in the past. And I'm trying again learn it, this time from here http://learn.javascript.ru/
+
+Let's see where I'll stop 
---
+++ part1/2/switch.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// switch (someValue) {
+//   case result1OfSomeValue:
+//     //some code
+//     break;
+//   case resultNOfSomeValue:
+//     //some code
+//       break;
+//   default:
+//     //some code
+// }
