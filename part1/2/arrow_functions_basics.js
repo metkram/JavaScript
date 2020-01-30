2e5b40f18abf71dc7fb253339aedd0d7152b75d0
Andrew T <metkraming@gmail.com>
2020-01-30 09:41:11 +0200

    add arrow_functions_basics.js

---
+++ part1/2/arrow_functions_basics.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let twoDigSum = (dig1, dig2) => dig1 + dig2;
+// the same as
+// function twoDigSum(dig1, dig2) {
+//   return dig1 + dig2;
+// }
+
+let sayHi = () => alert("Hi"); //arrow function without arguments
+
+let saySomething = (sentense) => {
+  let fullSentence = "You said: " + sentense;
+  return fullSentence;
+}
