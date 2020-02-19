e0433ab94e700b1cbc7f17b0565d228244d127f6
Andrew T <metkraming@gmail.com>
2020-02-19 22:05:55 +0200

    add JSON.parse and .toJSON to json.js file

--- part1/5/json.js
+++ part1/5/json.js
@@ -16,3 +16,39 @@ let json = JSON.stringify(obj);
 let json2 = JSON.stringify(obj, ["name"]); //particular chooshing
 //if Object links to another Object it'll help to form JSON without issues
 //alert(json2);
+
+
+//toJSON() works the same as toString() in objects
+
+// let obj1 = {
+//   name: "Bbb",
+//   toJSON() {
+//     return this.name;
+//   }
+// }
+// alert(JSON.stringify(obj1)); returns "Bbb" instead of full JSON file
+
+
+//JSON.parse(str, [reviver])
+
+let str1 = '{"name": "some name", "age": 22}';
+let obj2 = JSON.parse(str1);
+//alert(Object.keys(obj2));
+
+//[reviver] we use if we need restore fuction
+//e.g JSON.parse(jsonVar, function(key, value) {
+// if (key == "date") {
+//   return new Date(value);
+// }
+// return value;
+// });
+
+
+//tasks
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+let userJson = JSON.stringify(user);
+let user1 = JSON.parse(userJson);
