95320a1f9b0991046bec18df589a2ef56f56d1f9
Andrew T <metkraming@gmail.com>
2020-02-28 11:44:20 +0200

    add settimeout_setinterval.js file

---
+++ part1/6/settimeout_setinterval.js
@@ -0,0 +1,18 @@
+"use strict";
+//it'll first step to create first step by step game (28.02.20)
+
+//setTimeout(func without braces, time in mc, art1, arg2, etc.) - launch function one time
+//setInterval(func without braces, time in mc, art1, arg2, etc.) - many times
+//clearTimeout(name of timeout variable) - decline the timeout
+//clearInterval(name);
+
+function sayHi(name) {
+  alert(`hi ${name}`);
+}
+// let firstFunLaunch = setTimeout(sayHi, 5000, "Andrew"); // works, I've cheched
+
+// clearTimeout(firstFunLaunch); //works
+
+// let firtInterval = setInterval(sayHi, 2000, "Andrew"); //works
+//
+// clearInterval(firtInterval);
