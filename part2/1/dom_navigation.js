8aaf31a273d48924e7555fd06d7a8628f8a76dac
unknown <metkraming@gmail.com>
2020-04-27 15:48:00 +0300

    add dom_navigation.js file

---
+++ part2/1/dom_navigation.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// document.body.childNodes - all nodes
+// document.body.firstChild - first node
+// .lastChild - last node
+// .hasChilsNodes() - return true or false
+
+//convert ro array
+// Array.from(document.body.childNodes)
+
+//sibling
+// .nextSibling .previousSibling
+
+
+// document.body.children - return only elements without text nodes and children-children
+// .firstElementChild .lastElementChild .previousElementSibling .nextElementSibling .parentElement
