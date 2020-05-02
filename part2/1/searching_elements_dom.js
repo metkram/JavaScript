45c366a4e317d7ff5f72fde6a2b5bc805ad9f553
unknown <metkraming@gmail.com>
2020-05-02 20:14:35 +0300

    add searching_elements_dom.js file and some html and css

---
+++ part2/1/searching_elements_dom.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let div = document.getElementById("block");
+
+div.style.background = "purple";
+
+//or
+
+block_one.style.background = "yellow";
+
+console.log(document.querySelectorAll("div")); //all divs
+console.log(document.querySelector("div")); //first div
+
+//elem.matches(css) true\false
+//elem.closest(css) retunt ancestor
