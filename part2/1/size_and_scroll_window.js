9945d60445a01f9d8a16763cfa1436eb9e2f74d5
unknown <metkraming@gmail.com>
2020-05-23 17:08:54 +0300

    add size_and_scroll_window.js file

---
+++ part2/1/size_and_scroll_window.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//alert(document.body.clientHeight);
+
+//window.scrollBy(0, 100); doesn't work
+
+//document.body.style.overflow = "hidden";
+
+alert(document.body.clientWidth);
+// .clientHeight
+// .scrollHeight
+// .offsetHeight
